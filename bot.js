c4d94818be65862dc23091c68a50043722ed69de
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-11-12 18:41:53 +0700

    fix

--- bot.js
+++ bot.js
@@ -1,30 +1,34 @@
 const axios = require('axios');
-
 require('dotenv').config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const TELEGRAM_BOT_TOKEN_DARVAS = process.env.TELEGRAM_BOT_TOKEN_DARVAS;
 
-async function sendTelegramNotification(message) {
-  if (!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
-    console.error('Lỗi: TELEGRAM_BOT_TOKEN hoặc TELEGRAM_CHAT_ID chưa được thiết lập trong biến môi trường.');
+async function sendTelegramNotification(message, isDarvasRoom = false) {
+  if (!TELEGRAM_BOT_TOKEN || (!TELEGRAM_CHAT_ID && !TELEGRAM_BOT_TOKEN_DARVAS)) {
     return;
   }
 
-  const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+  // 3. Chọn chat_id dựa trên tham số isDarvasRoom
+  const bot = isDarvasRoom ? TELEGRAM_BOT_TOKEN_DARVAS : TELEGRAM_BOT_TOKEN;
+
+  if (!bot) {
+    return;
+  }
+
+  // 4. Sửa URL (loại bỏ dấu cách)
+  const url = `https://api.telegram.org/bot${bot}/sendMessage`;
+
   try {
     await axios.post(url, {
       text: message,
       chat_id: TELEGRAM_CHAT_ID,
       parse_mode: "Markdown"
     });
-    console.log('Notification sent to Telegram');
   } catch (error) {
     if (error.response) {
-      console.error("Telegram API Error:", error.response.status, error.response.statusText);
-      console.error("Response data:", error.response.data);
     } else {
-      console.error("Axios Error:", error.message);
     }
   }
 }
