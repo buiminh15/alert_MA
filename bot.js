5874fc3d5eb244eedeb87d8519b24516052fa7e8
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-11-12 19:06:06 +0700

    fix: bot

--- bot.js
+++ bot.js
@@ -7,15 +7,12 @@ const TELEGRAM_BOT_TOKEN_DARVAS = process.env.TELEGRAM_BOT_TOKEN_DARVAS;
 
 async function sendTelegramNotification(message, isDarvasRoom = false) {
   if (!TELEGRAM_BOT_TOKEN || (!TELEGRAM_CHAT_ID && !TELEGRAM_BOT_TOKEN_DARVAS)) {
+    console.log('📢 [bot.js:10]', !TELEGRAM_BOT_TOKEN || (!TELEGRAM_CHAT_ID && !TELEGRAM_BOT_TOKEN_DARVAS));
     return;
   }
-
+  console.log('📢 [bot.js:13]', 'running');
   // 3. Chọn chat_id dựa trên tham số isDarvasRoom
   const bot = isDarvasRoom ? TELEGRAM_BOT_TOKEN_DARVAS : TELEGRAM_BOT_TOKEN;
-
-  if (!bot) {
-    return;
-  }
 
   // 4. Sửa URL (loại bỏ dấu cách)
   const url = `https://api.telegram.org/bot${bot}/sendMessage`;
@@ -26,9 +23,12 @@ async function sendTelegramNotification(message, isDarvasRoom = false) {
       chat_id: TELEGRAM_CHAT_ID,
       parse_mode: "Markdown"
     });
+    console.log('📢 Notification sent to telegram');
   } catch (error) {
     if (error.response) {
+      console.log('📢 [bot.js:29]', 'error');
     } else {
+      console.log('📢 [bot.js:31]', 'error');
     }
   }
 }
